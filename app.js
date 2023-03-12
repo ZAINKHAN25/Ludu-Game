2cc13d0e45bb32be6e9ea50bbb14f62ec64dd4c7
ZAINKHAN25 <zainarfeen87@gmail.com>
2023-03-12 17:13:04 +0500

    I think final commit

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-var player1 = true;
-var player2 = false;
-var back1 = document.querySelector("#player1");
-var back2 = document.querySelector("#player2");
-var onecscore = document.querySelector("#dice-1");
-var twocscore = document.querySelector("#dice-2");
-var onetscore = document.querySelector("#onetscore")
-var twotscore = document.querySelector("#twotscore")
-var hold = document.querySelector("#hold")
-var rolldice = document.querySelector("#rolldice")
-var imags = document.querySelector("#imags")
-var newgame = document.querySelector("#newgame")
+// var player1 = true;
+// var player2 = false;
+// var back1 = document.querySelector("#player1");
+// var back2 = document.querySelector("#player2");
+// var onecscore = document.querySelector("#dice-1");
+// var twocscore = document.querySelector("#dice-2");
+// var onetscore = document.querySelector("#onetscore")
+// var twotscore = document.querySelector("#twotscore")
+// var hold = document.querySelector("#hold")
+// var rolldice = document.querySelector("#rolldice")
+// var imags = document.querySelector("#imags")
+// var newgame = document.querySelector("#newgame")
 
-newgame.addEventListener("click" , () => {
-    location.reload();
-})
-
-rolldice.addEventListener("click" , () firstplayer)
-function firstplayer(){
-    var autotgenerate = 
-}
+// newgame.addEventListener("click" , () => {
+//     location.reload();
+// })
 
 
+// function first(){
 // rolldice.addEventListener('click', Rolldice)
 // function Rolldice(){
 //     var autotgenerate = Math.round(Math.random() * 5 ) + 1
 //     imags.classList.remove('hidden')
 //     imags.src = `./assets/${autotgenerate}.png`
-//     if (autotgenerate == 1) {
+//     if (autotgenerate == 1 && player1 == true && player2 == false) {
+//         two()
+//         player1 = false;
+//         player2 = true;
 //         onecscore.textContent = 0;
 //     } else {
 //         onecscore.textContent = Number(onecscore.textContent) + autotgenerate
@@ -45,16 +44,20 @@ function firstplayer(){
 //         alert("Player 1 wins")
 //     }
 // }
+// }
 
 
-
+// function two(){
 // rolldice.addEventListener('click', Rolldice)
 // function Rolldice(){
 //     var autotgenerate = Math.round(Math.random() * 5 ) + 1
 //     imags.classList.remove('hidden')
 //     imags.src = `./assets/${autotgenerate}.png`
-//     if (autotgenerate == 1) {
+//     if (autotgenerate == 1 player2 == false && player1 == true) {
 //         twocscore.textContent = 0;
+//         first()
+//         player1 = true;
+//         player2 = false;
 //     } else {
 //         twocscore.textContent = Number(twocscore.textContent) + autotgenerate
 //     }   
@@ -72,3 +75,79 @@ function firstplayer(){
 //         alert("Player 2 wins")
 //     }
 // }
+// }
+// // rolldice.addEventListener("click" , () firstplayer)
+// // function firstplayer(){
+// //     var autotgenerate = 
+// // }
+
+
+// Define variables for game elements
+const player1Score = document.querySelector("#onetscore");
+const player2Score = document.querySelector("#twotscore");
+const player1CurrentScore = document.querySelector("#dice-1");
+const player2CurrentScore = document.querySelector("#dice-2");
+const rollButton = document.querySelector("#rolldice");
+const holdButton = document.querySelector("#hold");
+const resetButton = document.querySelector("#newgame");
+const diceImage = document.querySelector("#imags");
+
+let activePlayer = 1; // Initialize active player to player 1
+let currentScore = 0; // Initialize current score to 0
+
+// Function to switch active player
+function switchPlayer() {
+  // Reset current score and update UI
+  currentScore = 0;
+  if (activePlayer === 1) {
+    player1CurrentScore.textContent = 0;
+    activePlayer = 2;
+  } else {
+    player2CurrentScore.textContent = 0;
+    activePlayer = 1;
+  }
+}
+
+// Function to update current score and UI for active player
+function rollDice() {
+  // Generate random number between 1 and 6
+  const roll = Math.floor(Math.random() * 6) + 1;
+  // Update dice image and current score
+  diceImage.src = `./assets/${roll}.png`;
+  if (roll === 1) {
+    // Switch to other player's turn if roll is 1
+    switchPlayer();
+  } else {
+    // Update current score and UI
+    currentScore += roll;
+    if (activePlayer === 1) {
+      player1CurrentScore.textContent = currentScore;
+    } else {
+      player2CurrentScore.textContent = currentScore;
+    }
+  }
+}
+
+// Function to update total score and check for winner
+function hold() {
+  // Update total score and UI for active player
+  if (activePlayer === 1) {
+    player1Score.textContent = Number(player1Score.textContent) + currentScore;
+  } else {
+    player2Score.textContent = Number(player2Score.textContent) + currentScore;
+  }
+  // Check for winner
+  if (Number(player1Score.textContent) >= 30) {
+    alert("Player 1 wins!");
+  } else if (Number(player2Score.textContent) >= 30) {
+    alert("Player 2 wins!");
+  } else {
+    // Switch to other player's turn if no winner
+    switchPlayer();
+  }
+}
+
+// Event listeners for roll, hold, and reset buttons
+rollButton.addEventListener("click", rollDice);
+holdButton.addEventListener("click", hold);
+resetButton.addEventListener("click", () => location.reload());
